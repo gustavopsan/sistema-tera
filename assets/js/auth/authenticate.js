3c2250c73c15fb6f290cd664692b9baab421be1d
Gustavo P. Santana <gustavopsantana4@gmail.com>
2024-02-03 15:04:49 -0300

    iniciando lógica de login

--- assets/js/auth/authenticate.js
+++ assets/js/auth/authenticate.js
@@ -3,11 +3,27 @@ const password = document.getElementById('passwordInput');
 
 const buttonLogin = document.getElementById('btn-login');
 
-function authenticate() {
-    console.log({
+async function authenticate() {
+    const requestBody = JSON.stringify({
         email: email.value,
         password: password.value
     })
+
+    const requestData = await fetch(
+        `${BASEPATH}/user/auth`,
+        {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'ContentType': 'application/json'
+            },
+            body: requestBody
+        }
+    );
+
+    const response = await requestData.json();
+
+    console.log(response)
 }
 
 buttonLogin.addEventListener('click', authenticate);
