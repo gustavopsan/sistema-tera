0f1f5b46e82530d10866d14afce2da8a9a8677c0
Gustavo P. Santana <gustavopsantana4@gmail.com>
2024-02-12 23:51:31 -0300

    gerando link de recuperação de senha

--- assets/js/auth/requestPasswordRecovery.js
+++ assets/js/auth/requestPasswordRecovery.js
@@ -1,5 +1,7 @@
 const email = document.getElementById('emailInput');
 const buttonRequest = document.getElementById('buttonRequest');
+const messageElement = document.getElementById('error-message');
+const errorMessageContainer = document.getElementById('error-message-container');
 
 async function requestPasswordRecovery() {
     const requestBody = JSON.stringify({
@@ -20,7 +22,22 @@ async function requestPasswordRecovery() {
 
     const response = await requestData.json();
 
-    console.log(response);
+    //console.log(response);
+
+    if (response.status == "error") {
+        let message = 'Nenhuma conta encontrada com os dados informados'
+
+        messageElement.textContent = message;
+
+        errorMessageContainer.style.display = 'flex';
+
+        setTimeout(() => { 
+            errorMessageContainer.style.display = 'none'; 
+        }, 3000);
+    } else {
+        let resetPassLink = window.location.hostname + '/reset-password?uid=' + response.userId + '&t=' + response.token;
+        console.log(resetPassLink);
+    }
 }
 
 buttonRequest.addEventListener('click', requestPasswordRecovery);
