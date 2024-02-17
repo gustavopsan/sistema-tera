8ad0d39e3f453fd4401e1e40c47e0f26b143ac89
Gustavo P. Santana <gustavopsantana4@gmail.com>
2024-02-17 15:30:26 -0300

    Ajustando fluxo de recuperação de senha

--- assets/js/auth/resetPassword.js
+++ assets/js/auth/resetPassword.js
@@ -10,8 +10,22 @@ const errorMessageContainer = document.getElementById('error-message-container')
 
 async function resetPassword() {
 
-    if (passwordInput.value !== confirmPasswordInput.value) {
-    let message = 'Senha e confirmação não conferem. Tente novamente!'
+    console.log(passwordInput.value)
+
+    if (passwordInput.value == "" || confirmPasswordInput.value == "") {
+        let message = 'Senha ou confirmação não podem estar vazios. Tente novamente!'
+
+        messageElement.textContent = message;
+        messageElement.classList.add("error");
+        messageElement.classList.remove("success");
+
+        errorMessageContainer.style.display = 'flex';
+
+        setTimeout(() => { 
+            errorMessageContainer.style.display = 'none'; 
+        }, 3000);
+    } else if (passwordInput.value !== confirmPasswordInput.value) {
+        let message = 'Senha e confirmação não conferem. Tente novamente!'
 
         messageElement.textContent = message;
         messageElement.classList.add("error");
@@ -29,7 +43,37 @@ async function resetPassword() {
             password: passwordInput.value
         })
 
-        console.log(requestBody);
+        const requestData = await fetch(
+            `${BASEPATH}/user/resetpassword`,
+            {
+                method: 'POST',
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: requestBody
+            }
+        );
+
+        const response = requestData.json();
+
+        console.log(response);
+
+        if (response.status == "error") {
+            let message = 'Solicitação inválida ou expirada. Inicie uma nova solicitação.'
+
+            messageElement.textContent = message;
+            messageElement.classList.add("error");
+            messageElement.classList.remove("success");
+
+            errorMessageContainer.style.display = 'flex';
+
+            setTimeout(() => { 
+                window.location.pathname = '/esqueci-minha-senha';
+            }, 3000)
+        } else {
+            window.location.pathname = '/login';
+        }
     }
 
 }
