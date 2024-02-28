49b514cfa3a59f9efcc15f27d14af4881ef03b93
Gustavo P. Santana <gustavopsantana4@gmail.com>
2024-02-27 23:47:24 -0300

    Criando lógica de criação de crediário

--- assets/js/queries/creditQueries.js
+++ assets/js/queries/creditQueries.js
@@ -6,6 +6,8 @@ const nameInput = document.getElementById('nameInput');
 const creditValueNumber = document.getElementById('creditValue');
 const paymentsAmount = document.getElementById('paymentsAmount');
 const firstPaymentDate = document.getElementById('firstPaymentDate');
+const nameSpan = document.getElementById('nameAutocomplete');
+const idInput = document.getElementById('idInput');
 
 const finalValueSpan = document.getElementById('finalValueSpan');
 const paymentsAmountSpan = document.getElementById('paymentsAmountSpan');
@@ -26,13 +28,87 @@ function calculateValue() {
     paymentValueSpan.innerHTML = paymentValue.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
 
     paymentsAmountSpan.innerHTML = paymentsAmount.value;
+}
 
+function calculateDates() {
     var daysToSum = parseInt(paymentsAmount.value);
     var lastDate = new Date(firstPaymentDate.value);
     lastDate.setDate(lastDate.getDate() + daysToSum);
 
     firstPaymentDateSpan.innerHTML = formateAMerdaDaData(firstPaymentDate.value);
     lastPaymentDateSpan.innerHTML = lastDate.toLocaleString("pt-br").split(' ')[0];
+}
+
+var clientData = {
+    name: "",
+    clientId: ""
+}
+
+async function searchCustomers() {
+    const customerStr = nameInput.value;
+
+    const searchedCustomers = await fetch(
+        `${BASEPATH}/customer/search`,
+        {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ queryString: customerStr, sellerId: sellerId })
+        }
+    )
+
+    let response = await searchedCustomers.json();
+
+    if (customerStr == "") {
+        nameSpan.innerText = ""
+    }
+    else if (response) {
+        nameSpan.innerText = response[0].name
+
+        clientData.name = response[0].name;
+        clientData.clientId = response[0]._id;
+    }
+}
+
+function setData() {
+    nameInput.value = clientData.name;
+    idInput.value = clientData.clientId;
+    nameSpan.innerText = ""
+}
+
+async function createCredit() {
+    var requestBody = JSON.stringify(
+        {
+            sellerId: sellerId,
+            customerId: clientData.clientId,
+            value: parseFloat(creditValueNumber.value),
+            paymentsAmount: parseInt(paymentsAmount.value),
+            paymentsRemaing: parseInt(paymentsAmount.value)
+        }
+    );
+    
+    console.log(requestBody)
+
+    const newCredit = await fetch(
+        `${BASEPATH}/debits/create`,
+        {
+            method: 'POST', 
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: requestBody
+        }
+    );
+
+    let response = await newCredit.json();
+
+    if (response.debitId) {
+        alert("Crédito criado com sucesso! Redirecionando para listagem...");
+        window.location.href = "../"
+    }
 }
 
 async function listCredits() {
@@ -65,7 +141,7 @@ async function listCredits() {
                 <td>${(credit.totalValue / credit.paymentsAmount).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</td>
                 <td>${credit.payments.length} / ${credit.paymentsAmount}</td>
                 <td>${credit.valueRemaing.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</td>
-                <td><button class="pay-button">Pagar</button></td>
+                <td><button class="pay-button" data-debit-id="${credit.debitId}">Pagar</button></td>
             </tr>
             `
         } else {
@@ -75,6 +151,7 @@ async function listCredits() {
                 <td>${(credit.totalValue / credit.paymentsAmount).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</td>
                 <td>${credit.payments.length} / ${credit.paymentsAmount}</td>
                 <td>${credit.valueRemaing.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</td>
+                <td><button class="pay-button" data-debit-id="${credit.debitId}">Pagar</button></td>
             </tr>
             `
         }
@@ -89,3 +166,5 @@ async function listCredits() {
 if (creditList) {
     window.addEventListener('load', listCredits)
 }
+
+nameInput.addEventListener('focusout', setData)
