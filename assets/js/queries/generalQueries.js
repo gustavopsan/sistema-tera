bf8a792ca77cc9f86356137484f426cf79a37ce8
Gustavo P. Santana <gustavo@iMac-de-Gustavo.lan>
2024-03-19 23:04:39 -0700

    Ajustando tela inicial

---
+++ assets/js/queries/generalQueries.js
@@ -0,0 +1,43 @@
+const paymentsTodayEl = document.getElementById('payments-today');
+const paymentsWeekEl = document.getElementById('payments-week');
+const creditsTodayEl = document.getElementById('credits-today');
+const creditsWeekEl = document.getElementById('credits-week');
+const paymentsMonthEl = document.getElementById('payments-month');
+const creditsMonthEl = document.getElementById('credits-month');
+
+async function loadGeneralData() {
+
+    var requestBody = JSON.stringify(
+        {
+            sellerId: sellerId
+        }
+    );
+
+    const data = await fetch(
+        `${BASEPATH}/debits/loadgeneraldata`,
+        {
+            method: 'POST', 
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: requestBody
+        }
+    );
+
+    let response = await data.json();
+
+    console.log(response);
+
+    if (response) {
+        paymentsTodayEl.innerHTML = response.paymentsToday.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+        paymentsWeekEl.innerHTML = response.paymentsThisWeek.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+        paymentsMonthEl.innerHTML = response.paymentsThisMonth.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+        creditsTodayEl.innerHTML = response.creditsToday.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+        creditsWeekEl.innerHTML = response.creditsthisWeek.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+        creditsMonthEl.innerHTML = response.creditsthisMonth.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+    }
+
+}
+
+window.addEventListener('load', loadGeneralData);
