c000d20779aae85ba34f5e92d6f9693896592859
Gustavo P. Santana <gustavo@iMac-de-Gustavo.lan>
2024-04-09 23:23:38 -0700

    Excluindo despesas

--- assets/js/queries/expenses/listExpenses.js
+++ assets/js/queries/expenses/listExpenses.js
@@ -41,6 +41,35 @@ async function listExpenses() {
 
         expenseList.innerHTML += newExpense;
     })
+
+    addEevents();
+}
+
+async function removeExpense(event) {
+    var expenseId = event.target.parentNode.dataset.expenseId;
+    
+    const removedExpense = await fetch(
+        `${BASEPATH}/expense/remove`,
+        {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                expenseId: expenseId
+            })
+        }
+    );
+
+    let response = await removedExpense.json();
+
+    listExpenses();
+}
+
+function addEevents() {
+    var removeButtons = document.querySelectorAll(".expense-button");
+    removeButtons.forEach(button => button.addEventListener('click', removeExpense));
 }
 
 window.addEventListener('load', listExpenses);
