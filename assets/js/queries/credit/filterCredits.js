ac661d298610c95138e65cc78042874a32a9896e
Gustavo P. Santana <gustavopsantana4@gmail.com>
2024-06-01 23:11:24 -0300

    Ajustando listagem de débitos

--- assets/js/queries/credit/filterCredits.js
+++ assets/js/queries/credit/filterCredits.js
@@ -56,7 +56,7 @@ async function filterCredits(initialDate, finalDate) {
         newPayment = `
             <tr>
                 <td>${formateAMerdaDaData(parsedDate)}</td>
-                <td>${payment.clientName}</td>
+                <td>${payment.clientName.split(" ")[0]}</td>
                 <td>R$ ${payment.paymentValue}</td>
             </tr>
         `;
